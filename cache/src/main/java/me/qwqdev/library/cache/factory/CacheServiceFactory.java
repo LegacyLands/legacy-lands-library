81ec7ba6926ae2a3e7324272641cf65faa0ad061
QwQ-dev <qwqdev@gmail.com>
2024-12-20 16:25:59 +0800

    docs: CacheServiceFactory javadoc.

--- cache/src/main/java/me/qwqdev/library/cache/factory/CacheServiceFactory.java
+++ cache/src/main/java/me/qwqdev/library/cache/factory/CacheServiceFactory.java
@@ -4,10 +4,19 @@ import me.qwqdev.library.cache.service.CacheService;
 import me.qwqdev.library.cache.service.QueryCacheInterface;
 
 /**
- * @author qwq-dev
+ * A factory class for creating instances of CacheService.
+ *
  * @since 2024-12-20 15:37
  */
 public class CacheServiceFactory {
+    /**
+     * Creates a CacheService with the specified QueryCacheInterface implementation.
+     *
+     * @param queryCacheInterface the QueryCacheInterface implementation to be used by the CacheService
+     * @param <K>                 the type of cache key
+     * @param <V>                 the type of cache value
+     * @return a new instance of CacheService
+     */
     public static <K, V> CacheService<K, V> create(QueryCacheInterface<K, V> queryCacheInterface) {
         return new CacheService<>(queryCacheInterface);
     }
