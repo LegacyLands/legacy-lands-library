c7b02e76599b756c7db4cd5511f34a5f4dc9fa53
QwQ-dev <qwqdev@gmail.com>
2025-03-27 19:25:14 +0800

    published: 1.0.4-SNAPSHOT.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     id("java-library") apply true
 
     // Fairy framework plugin
-    id("io.fairyproject") version "0.7.12b1-SNAPSHOT" apply false
+    id("io.fairyproject") version "0.7.13b3-SNAPSHOT" apply false
 
     // Dependency management plugin
     id("io.spring.dependency-management") version "1.1.0"
